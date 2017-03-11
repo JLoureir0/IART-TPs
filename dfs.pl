4b3706399ddaef6647637e67f21cbe50f4c5219a
João Loureiro <joao.a.g.loureiro@gmail.com>
2017-03-11 19:13:23 +0000

    Refactoring on the dfs function

--- dfs.pl
+++ dfs.pl
@@ -1,10 +1,10 @@
-dfs([S|Path],_,[S|Path]):-final_state(S).
+dfs([S|Path],_,[S|Path]):-
+  final_state(S).
 
 dfs([S|Path],Visited,Sol):-
   successor(S,S2),
   \+ member(S2,Visited),
-  append([S2],[S|Path],L),
-  dfs(L,[S2|Visited],Sol).
+  dfs([S2|[S|Path]],[S2|Visited],Sol).
 
 solve_dfs(Sol):-
   initial_state(Si),
